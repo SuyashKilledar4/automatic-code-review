fc017374c9396e7aa21dab2e4097a1f32322d4e9
SuyashKilledar4 <killedarsuyash@gmail.com>
2025-07-09 16:29:21 +0530

    Update export statement.

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,14 @@
 
 const unusedVariable = 42
 
-return function badFunctionName(){
+export function badFunctionName(){
 let x=5
 let y = 10
 console.log("Sum is: "+(x+y))
 return x+y
 }
 
-return function anotherFunction(param1: any, param2: any){
+export function anotherFunction(param1: any, param2: any){
     let result=param1+param2
     console.log("Result: "+result)
     return result
@@ -20,30 +20,30 @@ for(let i=0;i<someArray.length;i++){
 console.log("Value: "+someArray[i])
 }
 
-return function longLineFunction() {
+export function longLineFunction() {
     const message = "This is a very long line that should probably be broken up into multiple lines because it exceeds the typical maximum line length that linters usually enforce.";
     console.log(message)
 }
 
-return function inconsistentSpacing ( a:number , b : number ) : number {
+export function inconsistentSpacing ( a:number , b : number ) : number {
 return a+b
 }
 
-return function unusedParams(a: number, b: number, c: number): number {
+export function unusedParams(a: number, b: number, c: number): number {
     return a + b
 }
 
-return function unreachableCode(): number {
+export function unreachableCode(): number {
     return 1
     console.log("This will never run")
 }
 
-return function magicNumbers(): number {
+export function magicNumbers(): number {
     const radius = 5
     return 3.14 * radius * radius
 }
 
-return function missingReturnType(a, b){
+export function missingReturnType(a, b){
     return a - b
 }
 
@@ -53,26 +53,26 @@ const obj = {
     location:"Earth"
 }
 
-return function improperNaming(){
+export function improperNaming(){
     let camel_case = "bad naming"
     console.log(camel_case)
 }
 
-return function nestedFunction(){
+export function nestedFunction(){
     function inner(){
         console.log("Inner function")
     }
     inner()
 }
 
-return function multipleIssues( a:any , b : any ){
+export function multipleIssues( a:any , b : any ){
     let unused = 123
     let result = a+b
     console.log("Result is "+result)
     return result
 }
 
-return const arrowFunc = () => { console.log("Arrow function") }
+export const arrowFunc = () => { console.log("Arrow function") }
 
 arrowFunc()
 
